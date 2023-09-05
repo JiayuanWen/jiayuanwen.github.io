60ea3762d5517c92babe0dfc8f8fe57652992856
Jiayuan Wen <91281147+JiayuanWen@users.noreply.github.com>
2023-09-05 04:42:37 -0400

    Added disclaimer at loading screen

--- js/Main.js
+++ js/Main.js
@@ -183,6 +183,13 @@ loadingManager.onProgress = function(url, loaded, total) {
 loadingManager.onError = function (url) {
 	alert("If site does not load, try refresh.");
 }
+async function showDisclaimer() {
+	let delayer = await delay(5700);
+	if (loadingBar.value != 100) {
+		document.getElementById("loading-disclaimer").style.visibility = "visible";
+	}
+}
+showDisclaimer();
 
 // Execute on loading complete
 const loadingScreen = document.querySelector('.loading-screen');
