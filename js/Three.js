fba4d8326f014eec41bdf426a5327fd6b82c34c7
JiayuanWen <91281147+jiayuanwen@users.noreply.github.com>
2023-10-31 23:26:11 -0400

    Fixed Three.js render not working

--- js/Three.js
+++ js/Three.js
@@ -68,6 +68,15 @@ $(document).ready(function() {
 			threeElement.style.transition = "0s";
 			threeElement.style.pointerEvents = "auto";
 			
+			// Enable rendering
+			animateStart();
+
+			camera.aspect = 1200 / 700;
+			camera.updateProjectionMatrix(); 
+
+			renderer.setSize( 1200, 700 ); 
+			composer.setSize( 1200, 700 );
+			renderer.setPixelRatio( window.devicePixelRatio );
 		  }
 		  else {
 			//threeShortcut.style.color = "#ffffff";
@@ -97,6 +106,9 @@ $(document).ready(function() {
 		threeElement.style.pointerEvents = "none";
 	});
 	document.getElementById("threejs-close").addEventListener('click', function(){ 
+		//Stop rendering
+		animateStop();
+
 		threeShortcut.style.color = "#ffffff";
 		threeElement.style.transition = "0.3s";
 		threeElement.style.opacity = 0; 
@@ -187,9 +199,9 @@ const cam_renderMax = 1000
 const camera = new THREE.PerspectiveCamera(cam_fov, 1200 / 700, cam_renderMin, cam_renderMax);
 
 // Camera default position
-const camera_default_x = 8; 
-const camera_default_y = 8;
-const camera_default_z = 8;
+const camera_default_x = 12; 
+const camera_default_y = 12;
+const camera_default_z = 12;
 //camera.lookAt(0,0,0);
 
 // Creating orbitcontrol to have our camera to orbit around the model.
@@ -228,11 +240,249 @@ const zoomInAnimation = async() => {
 	/*
 	document.getElementById("fiber-lamp").style.paddingRight = "50vw";
 	document.getElementById("fiber-lamp").style.paddingTop = "0vh";
-	*/
+	
 	document.getElementById("fiber-lamp").style.paddingRight = "0vw";
 	document.getElementById("fiber-lamp").style.paddingTop = "0vh";
+	*/
 }
 
+//
+camera.position.set(camera_default_x,camera_default_y,camera_default_z);
 
 //----------------------------------------------------------------------------------------- Firefox & Safari WebXR compatibility
 const polyfill = new WebXRPolyfill();
+
+//----------------------------------------------------------------------------------------- 3D models
+// Fiber Optic Lamp
+const fiber_lamp = new THREE.Object3D();
+	// Body
+	const fiber_lamp_body = new GLTFLoader(/*loadingManager*/);
+	fiber_lamp_body.load('3d_models/fiber_lamp/fiber-optic-lamp-body.gltf', function(gltf) {
+		const model_root = gltf.scene;
+		fiber_lamp.add(model_root);
+	}, 
+	// Called when loading is in progresses
+	function (xhr) {
+		if (( xhr.loaded / xhr.total * 100 ) == 100 && dev_output) {
+			console.log("Fiber Lamp: ");
+			console.log(fiber_lamp);
+			console.log(" ");
+		}
+	}, 
+	// Called when loading has errors
+	function (error) {
+		console.log("Fiber Lamp Object3D error: ");
+		console.log(error);
+		console.log(" ");
+	})
+
+	// Glow
+	const fiber_lamp_glow = new GLTFLoader(/*loadingManager*/);
+	fiber_lamp_glow.load('3d_models/fiber_lamp/fiber-optic-lamp-glow.gltf', function(gltf) {
+		const model_root = gltf.scene;
+		fiberColorChange(model_root);
+		fiber_lamp.add(model_root);
+	}, 
+	// Called when loading is in progresses
+	function (xhr) {
+		if (( xhr.loaded / xhr.total * 100 ) == 100 && dev_output) {
+			console.log("Fiber Glow: ");
+			console.log(fiber_lamp);
+			console.log(" ");
+		}
+	}, 
+	// Called when loading has errors
+	function (error) {
+		console.log("Fiber Lamp Object3D error: ");
+		console.log(error);
+		console.log(" ");
+	})
+
+// Color changing fibers
+var red = 255; 
+var green = 0;	
+var blue = 0;	
+var color_stage = 1;
+async function fiberColorChange(model_root) {
+	while (true) {
+		// Cycle through all non-monochrome colors
+		if (color_stage == 1) {
+			blue += 1;
+			if (blue >= 254) {
+				color_stage++;
+			}
+		}
+		if (color_stage == 2) {
+			red -= 1;
+			if (red <= 1) {
+				color_stage++;
+			}
+		}
+		if (color_stage == 3) {
+			green += 1;
+			if (green >= 254) {
+				color_stage++;
+			}
+		}
+		if (color_stage == 4) {
+			blue -= 1;
+			if (blue <= 1) {
+				color_stage++;
+			}
+		}
+		if (color_stage == 5) {
+			red += 1;
+			if (red >= 254) {
+				color_stage++;
+			}
+		}
+		if (color_stage == 6) {
+			green -= 1;
+			if (green <= 1) {
+				color_stage = 1;
+			}
+		}
+		//console.log("Color changed");
+		model_root.traverse((child, i) => {
+			if (child.isMesh) {
+			  child.material = new THREE.MeshBasicMaterial({ color: rgbToHex(red, green, blue)});;
+			}
+		 });
+		 let delayers = await delay(8.2);
+	}
+}
+
+scene.add(fiber_lamp);
+
+//---------------------------------------------------------------------------------------- Lights
+
+
+//---------------------------------------------------------------------------------------- Composition
+const renderPass = new RenderPass( scene, camera );
+
+var renderTarger_params = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat, stencilBuffer: false };
+var renderTarget = new THREE.WebGLRenderTarget( 
+	window.innerWidth, 
+	window.innerHeight, 
+	renderTarger_params
+);
+
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+bloomPass.threshold = 0;
+bloomPass.strength = 1;
+bloomPass.radius = 0.1;
+
+let composer = new EffectComposer( renderer, renderTarget );
+composer.addPass( renderPass );
+composer.addPass( bloomPass );
+
+//---------------------------------------------------------------------------------------- Window resize handler
+// Updates render area on device orientation change (Mobile)
+/*
+function onOrientationChange() {
+	setTimeout(function(){
+		switch(window.orientation) 
+		{  
+		case -90:
+		case 90:
+			console.log('Device in landscape mode');
+			console.log( 'width: ' + window.innerWidth, 'height: ' + window.visualViewport.innerHeight );
+			
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix(); 
+
+			// Setting the size of renderer for landscape
+			renderer.setSize( window.innerWidth, window.innerHeight ); 
+			composer.setSize( window.innerWidth, window.innerHeight );
+			renderer.setPixelRatio( window.devicePixelRatio );
+			break; 
+		default:
+			console.log('Device in portrait mode');
+			console.log( 'width: ' + window.innerWidth, 'height: ' + window.innerHeight );
+
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix(); 
+
+			// Setting the size of renderer for portrait
+			renderer.setSize( window.innerWidth, window.innerHeight ); 
+			composer.setSize( window.innerWidth, window.innerHeight );
+			renderer.setPixelRatio( window.devicePixelRatio );
+			break; 
+		}
+	},200);
+}
+window.addEventListener('orientationchange', onOrientationChange);
+*/
+
+// Updates render area when resizing window (Desktop)
+/*
+function onWindowResize(){
+	//console.log( 'width: ' + document.documentElement.clientWidth, 'height: ' + window.innerHeight );
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix(); 
+
+	renderer.setSize( window.innerWidth, window.innerHeight ); 
+	composer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setPixelRatio( window.devicePixelRatio );
+}
+window.addEventListener( 'resize', onWindowResize, false );
+*/
+
+//---------------------------------------------------------------------------------------- Finalize renderer 
+let animate_request;
+let frames = 0, prevTime = performance.now();
+
+function animate() {
+
+	animate_request = undefined;
+	
+	delta = clock.getDelta();
+
+	// Animation controllers
+	fiber_lamp.rotation.y += 0.055 * delta;
+
+	// Allow user to control camera
+	camera_controls.update();
+
+	// FPS
+    frames ++;
+    const time = performance.now();
+    
+    if ( time >= prevTime + 1000 ) {
+    
+    	console.log( Math.round( ( frames * 1000 ) / ( time - prevTime ) ) );
+      
+      frames = 0;
+      prevTime = time;
+      
+    }
+
+	// Apply post-effects
+	//renderer.clear();
+	composer.render();
+	
+	// Render the 3D scene
+	//renderer.render(scene, camera);
+
+    // Required by WebXR
+	//renderer.setAnimationLoop(animate);
+
+	// Use this if only Three.js no WebXR
+	animateStart(); 
+	
+
+}
+
+function animateStart() {
+	if (!animate_request) {
+		animate_request = requestAnimationFrame(animate); 
+	 }
+}
+
+function animateStop() {
+	if (animate_request) {
+		cancelAnimationFrame(animate_request);
+		animate_request = undefined;
+	 }
+}
